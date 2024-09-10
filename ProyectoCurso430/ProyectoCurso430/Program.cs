bab6cb1edb6bd09a8ea40773c767599bbcb4b991
carkotx <94654612+carkotx@users.noreply.github.com>
2024-09-10 12:07:09 -0300

    primer commit

---
+++ ProyectoCurso430/ProyectoCurso430/Program.cs
@@ -0,0 +1,38 @@
+﻿
+public class Usuario
+{
+    public int Id;
+    public string Nombre;
+    public string Apellido;
+    public string NombreUsuario;
+    public string Contraseña;
+    public string Mail;          
+}
+
+// Clase Producto
+public class Producto
+{
+    public int Id;
+    public string Descripcion;
+    public decimal Costo;
+    public decimal PrecioVenta;
+    public int Stock;
+    public int IdUsuario;     
+}
+
+// Clase ProductoVendido
+public class ProductoVendido
+{
+    public int Id;
+    public int IdProducto;
+    public int Stock;
+    public int IdVenta;
+}
+
+// Clase Venta
+public class Venta
+{
+    public int Id;
+    public string Comentarios;
+    public int IdUsuario;   
+}
